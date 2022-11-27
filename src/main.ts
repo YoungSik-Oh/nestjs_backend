95e989ebccabf2ef72ba202d3d53c61f1c462f6f
youngsik <dudtlr9308@jm-soft.co.kr>
2022-11-27 16:20:10 +0900

    cors 설정 및, compile 할 때 yml, yaml, xml 파일도 build 해서 dist 파일에 넣는ㄷㅏ

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
   await app.listen(3000);
 }
 bootstrap();
